166edb6644e8d317af92e6dee99615ed8a6dac57
unknown <xmarsiancax@mail.ru>
2021-06-10 18:13:17 +0300

    added img on local file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import s from "./Header.module.css";
+import logo from "../../images/stringNaruto.png";
+import logoMain from "../../images/MainAva.jpg";
 
 const Header = () => {
     return <header className={s.header}>
         <div className={s.imgOne}>
             <img
-                src="https://csru.ru/wp-content/uploads/posts/2012-03/thumbs/csru.ru_logotip-naruto.jpg"
+                src={logoMain}
                  alt="Naruto logo"/>
         </div>
         <div className={s.imgTwo}>
             <img
-                src="https://dere.shikimori.one/system/user_images/original/448121/1115354.jpg"
+                src={logo}
                 alt="Naruto text"/>
 
         </div>
