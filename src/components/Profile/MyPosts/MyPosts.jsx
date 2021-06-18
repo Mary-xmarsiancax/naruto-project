93082ba3296438e86480c3faf490c0c7676b436c
marsianca <xmarsiancax@mail.ru>
2021-06-18 14:46:44 +0300

    добавление метода map и создание новых компонентов внутри Dialogs.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,28 +3,29 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
-    let NewPostData=[
-        {id:1, message: "Hello!",likesCounte:"17"},
-        {id:1, message: "How are you?",likesCounte:"20"},
-        {id:1, message: "I am fine!I am under the water",likesCounte:"23"},
+    let NewPostData = [
+        {id: 1, message: "Hello!", likesCounte: "17"},
+        {id: 1, message: "How are you?", likesCounte: "20"},
+        {id: 1, message: "I am fine!I am under the water", likesCounte: "23"},
     ]
+    let MyPostsElements = NewPostData.map
+    (p => <Post message={p.message} likesCounte={p.likesCounte}/>);
+
     return (
-    <div className={s.myPostsFromHeader}>
-        <h2>My Posts</h2>
-        <div>
+        <div className={s.myPostsFromHeader}>
+            <h2>My Posts</h2>
             <div>
-                <textarea>New Post</textarea>
+                <div>
+                    <textarea>New Post</textarea>
+                </div>
+                <div>
+                    <button>Add Post</button>
+                </div>
+                <div className={s.posts}>
+                    New Post
+                    {MyPostsElements}
+                </div>
             </div>
-            <div>
-                <button>Add Post</button>
-            </div>
-        <div className={s.posts}>
-            New Post
-            <Post message={NewPostData[0].message}  likesCounte={NewPostData[0].likesCounte}/>
-            <Post message={NewPostData[1].message}  likesCounte={NewPostData[1].likesCounte}/>
-            <Post message={NewPostData[2].message}  likesCounte={NewPostData[2].likesCounte}/>
-        </div>
-        </div>
-    </div>)
+        </div>)
 }
 export default MyPosts;
