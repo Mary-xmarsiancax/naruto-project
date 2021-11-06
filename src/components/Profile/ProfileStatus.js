860ce6b6379b3088717d8402d5b70aef30b71696
marsianca <xmarsiancax@mail.ru>
2021-11-06 11:14:57 +0300

    added submit to onBlur on status

--- src/components/Profile/ProfileStatus.js
+++ src/components/Profile/ProfileStatus.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import s from "./Profile.module.css";
+import {Field, Form, Formik} from "formik";
 
 class ProfileStatus extends React.Component {
-    state = {
-        editMode: false,
-        status: this.props.status
+    formikRef = React.createRef();
+    inputRef = React.createRef();
+
+    constructor() {
+        super();
+        this.state = {
+            editMode: false,
+        }
     }
     activateEditMode = () => {
         this.setState({
             editMode: true
-        })
+        });
+
+        setTimeout(() => this.inputRef.current.select(), 0);
+        this.showJSCycle();
     }
-    deactivateEditMode = () => {
+
+    showJSCycle() {
+        console.log("START");
+        setTimeout(() => console.log("SET TIMEOUT"));
+        Promise.resolve().then(() => console.log("PROMISE"));
+        console.log("END")
+    }
+
+    onSubmitStatus = (formData) => {
         this.setState({
             editMode: false
         })
-        this.props.updateUsersStatus(this.state.status)
-    }
-    onStatusChange = (e) => {
-        this.setState({
-            status: e.currentTarget.value
-        })
+        this.props.updateUsersStatus(formData.status)
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
@@ -31,11 +43,25 @@ class ProfileStatus extends React.Component {
         }
     }
 
+    onBlur = () => {
+        this.formikRef.current.submitForm();
+    }
+
     render() {
         return (<div>
                 {this.state.editMode ?
-                    <div><input onBlur={this.deactivateEditMode}
-                                onChange={this.onStatusChange} value={this.state.status} type="string"/></div>
+                    <Formik innerRef={this.formikRef}
+                        initialValues={{
+                            status: this.props.status
+                        }}
+                        onSubmit={this.onSubmitStatus}
+                    >
+                        <Form>
+                            <div>
+                                <Field innerRef={this.inputRef} id="status" name="status" placeholder="add status" onBlur={this.onBlur}/>
+                            </div>
+                        </Form>
+                    </Formik>
                     : <div className={s.usersStatus}
                            onDoubleClick={this.activateEditMode}>{this.props.status || "no status yet"}</div>}
             </div>
