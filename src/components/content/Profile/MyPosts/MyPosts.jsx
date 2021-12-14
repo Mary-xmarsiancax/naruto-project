3638a769d6e966f09acd8666e3617ed6fe45ba0d
marsianca <xmarsiancax@mail.ru>
2021-12-14 19:51:26 +0300

    added method deletePost to MyPosts.jsx

--- src/components/content/Profile/MyPosts/MyPosts.jsx
+++ src/components/content/Profile/MyPosts/MyPosts.jsx
@@ -7,7 +7,6 @@ import {validatePost} from "../../../common/validate/validate";
 
 const MyPosts = (props) => {
     const [selectedId, setSelectedId] = useState(undefined)
-    console.log(selectedId);
     let myPostsElements = props.newPostData.map
     (p => <div  className={selectedId===p.id?s.selectedPostStyle:""} onClick={() => {
 
@@ -18,18 +17,15 @@ const MyPosts = (props) => {
     );
 
     const onSelectedPost = (postId) => {
-        console.log(postId);
         setSelectedId(postId)
-        console.log(postId);
     }
 
     const onSubmitPost = (formData) => {
         props.addPost(formData);
     }
 
-    const onDeletePost = (selectedId=1) => {
-        console.log(selectedId);//undefined
-        props.deletePost(selectedId)
+    const onDeletePost = (postId) => {
+        props.deletePost(postId)
     }
 
     return (
@@ -54,7 +50,7 @@ const MyPosts = (props) => {
                     )}
                 </Formik>
                 {props.isOwner &&
-                <button onClick={() => onDeletePost()}>Delete post</button>}
+                <button onClick={() => onDeletePost(selectedId)}>Delete post</button>}
                 <div className={s.posts}>
                     New Post
                     {myPostsElements}
