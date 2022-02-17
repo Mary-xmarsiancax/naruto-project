b6eadc515a3fa5a5bd198cfa3643cde525b33d56
marsianca <xmarsiancax@mail.ru>
2022-02-17 19:44:13 +0300

    added images

--- src/components/content/Music/MusicContainer.jsx
+++ src/components/content/Music/MusicContainer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Music from "./Music";
 import {compose} from "redux";
-import withAuthRedirect from "../../common/hocRedirect/withAuthRedirect";
 
 const MusicContainer = (props) => {
     return (
